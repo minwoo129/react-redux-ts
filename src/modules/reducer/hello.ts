a380ee9e9b1295b25a0586350917179bd9db8240
minwoo129 <key0129mw@naver.com>
2022-11-09 12:46:39 +0900

    hello reducer 생성

---
+++ src/modules/reducer/hello.ts
@@ -0,0 +1,24 @@
+import { createReducer } from "typesafe-actions";
+import { HelloActionType } from "../actions";
+import { HelloStateType } from "../initialStates/initialStateType";
+import { HelloState as initialState } from "../initialStates/initialState";
+import { ADD_COUNT, DEC_COUNT } from "../actionTypes/hello";
+
+export default createReducer<HelloStateType, HelloActionType>(initialState, {
+  [ADD_COUNT]: (state, action) => {
+    const newState: HelloStateType = {
+      ...state,
+      count: action.payload,
+    };
+
+    return newState;
+  },
+  [DEC_COUNT]: (state, action) => {
+    const newState: HelloStateType = {
+      ...state,
+      count: action.payload,
+    };
+
+    return newState;
+  },
+});
