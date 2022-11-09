10266343e7e7e75ef52c41e3e2a0ef6951930f7a
minwoo129 <key0129mw@naver.com>
2022-11-09 13:19:33 +0900

    App.tsx ui 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent, MouseEvent } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./modules/reducer";
 
-function App() {
+const App = () => {
+  const count = useSelector((state: RootState) => state.hello.count);
+
+  const [txt, setTxt] = useState<string>("");
+
+  const onChangeTxt = (e: ChangeEvent<HTMLInputElement>) => {
+    setTxt(e.target.value);
+  };
+
+  const onClickAdd = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log("txt: ", txt);
+  };
+
+  const onClickDec = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log("txt: ", txt);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={txt} type={"number"} onChange={onChangeTxt} />
+      <button onClick={onClickAdd}>덧셈</button>
+      <button onClick={onClickDec}>뺄셈</button>
+
+      <div>{count}</div>
     </div>
   );
-}
+};
 
 export default App;
